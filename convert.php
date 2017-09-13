8664405709950f5d9d283b14a790a1797ed04bd0
Bobby Iliev <bobby@bobbyiliev.com>
2017-09-13 16:37:48 +0300

    Update convert.php

--- convert.php
+++ convert.php
@@ -1,8 +1,80 @@
 <!DOCTYPE html>
 <html>
    <head>
-      <link rel="stylesheet" type="text/css" href="css/style.css">
-      <head>
+	<style>
+	/* Style inputs with type="text", select elements and textareas */
+	input[type=text1], select, textarea {
+	    //margin-right: auto; margin-left: auto; width: 40%;
+	    width: 100%; /* Full width */
+	    padding: 8px; /* Some padding */  
+	    border: 1px solid #ccc; /* Gray border */
+	    border-radius: 4px; /* Rounded borders */
+	    box-sizing: border-box; /* Make sure that padding and width stays in place */
+	    margin-top: 4px; /* Add a top margin */
+	    margin-bottom: 8px; /* Bottom margin */
+	    resize: vertical /* Allow the user to vertically resize the textarea (not horizontally) */
+	}
+
+	/* Style the submit button with a specific background color etc */
+	input[type=submit] {
+	    background-color: #4CAF50;
+	    color: white;
+	    padding: 10px 18px;
+	    border: none;
+	    border-radius: 4px;
+	    cursor: pointer;
+	}
+
+	/* When moving the mouse over the submit button, add a darker green color */
+	input[type=submit]:hover {
+	    background-color: #45a049;
+	}
+
+	/* Add a background color and some padding around the form */
+	.container {
+	    margin-right: auto; margin-left: auto; width: 40%;
+	    border-radius: 5px;
+	    background-color: #f2f2f2;
+	    padding: 10px;
+	}
+
+	.text {
+	    align: center
+	    width: 160%;
+	    padding: 4px 8px;
+	    margin: 6px 0;
+	    box-sizing: border-box;
+	    border: 1px solid #555;
+	}
+
+	.message {
+	    margin-right: auto; margin-left: auto; width: 40%;
+	    text: bold;
+	    padding: 12px; /* Some padding */
+	    border: 1px solid #ccc; /* Gray border */
+	    border-radius: 4px; /* Rounded borders */
+	    box-sizing: border-box; /* Make sure that padding and width stays in place */
+	    margin-top: 6px; /* Add a top margin */
+	    margin-bottom: 16px; /* Bottom margin */
+
+	}	
+
+	.downloadme {
+	    margin-right: auto; margin-left: auto; width: 40%;
+	    text: bold;
+	}
+	
+	download {
+	    background-color: #4CAF50;
+	    color: white;
+	    padding: 4px 8px;
+	    border: none;
+	    border-radius: 4px;
+	    cursor: pointer;
+	    float: right;
+	}
+	</style>
+   <head>
          <div class="container">
             <form method="post">
                <div class="ssl1">
